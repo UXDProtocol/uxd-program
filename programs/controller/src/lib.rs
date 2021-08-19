993705be707b213ec7d5df75fe9c88e169aab70e
hanako mumei <81144685+2501babe@users.noreply.github.com>
2021-08-19 20:22:47 +0000

    some notes

--- programs/controller/src/lib.rs
+++ programs/controller/src/lib.rs
@@ -17,6 +17,37 @@ const PROXYSEEDWORD: &[u8] = b"PROXY";
 pub mod controller {
     use super::*;
 
+    // MANGO API IN BRIEF
+    // shit we care about:
+    // * init account (good for a whole group aka a set of markets that can be xmargined)
+    // * deposit (coin into account)
+    // * withdraw (coin from account)
+    // * place perp order (self explanatory. order type comes from serum i think)
+    // * cancel perp order (their id and our id versions exist)
+    // * settle pnl (takes two accounts and trues up)
+    // settle is necessary but kinda weird in that like, you need to find a loser to match your winner
+    //
+    // shit we might:
+    // * add to basket ("add a spot market to account basket" never made clear wtf this is)
+    // * borrow (unclear if we need to borrow to short? prolly not...)
+    // * place spot order (this is just a serum passthrough)
+    // * cancel spot order (as above)
+    // * settle funds ("settle funds from serum dex open orders" maybe just serum passthrough?)
+    // * settle borrow (only if we use borrow
+    // the point of serum calls is they can use the money in mango accounts
+    // but... i dont think we need to mess w spot
+    //
+    // flow... user deposits btc, we send to mango
+    // open a equiv sized short position sans whatever amount for liquidation protection
+    // once the position is open it theoretically has a fix dollar value
+    // (sans execution risk, sans funding, sans liquidation buffer)
+    // this is the amount of uxd we mint and return to the user
+    // then redemption of uxd for the underlying means we... burn uxd
+    // close out an equivalent amount of position in the coin they want
+    // settle pnl, withdraw coin, deliver to depository, give user redeemables
+    // important that all trasaction costs and price differences *must* be passed onto the user
+    // otherwise we open ourselves up to all kind of insane arbitrage attacks
+    // since uxd *must* be fungible we cannot maintain accounts for individuals
 
     /////// Instruction functions ///////
 
