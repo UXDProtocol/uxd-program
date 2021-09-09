c3a013b61b742b4fb0c1fd5b85fc4aa3b7ad0b46
hanako mumei <81144685+2501babe@users.noreply.github.com>
2021-09-09 22:32:53 +0000

    register client code

--- app/index.js
+++ app/index.js
@@ -21,39 +21,46 @@ const TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 const ASSOC_TOKEN_PROGRAM_ID = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
 
 const TXN_COMMIT = "processed";
-const TXN_OPTS = {commitment: TXN_COMMIT, preflightCommitment: TXN_COMMIT, skipPreflight: true};
+const TXN_OPTS = {commitment: TXN_COMMIT, preflightCommitment: TXN_COMMIT, skipPreflight: false};
 
-const depositMintKey = new anchor.web3.PublicKey(COIN_MINT);
+const coinMintKey = new anchor.web3.PublicKey(COIN_MINT);
 const tokenProgramKey = new anchor.web3.PublicKey(TOKEN_PROGRAM_ID);
 const assocTokenProgramKey = new anchor.web3.PublicKey(ASSOC_TOKEN_PROGRAM_ID);
 
 const provider = anchor.Provider.local();
 anchor.setProvider(provider);
 
+// simple shorthand
+function findAddr(seeds, programId) {
+    return anchor.utils.publicKey.findProgramAddressSync(seeds, programId)[0];
+}
+
 // derives the canonical token account address for a given wallet and mint
-async function findAssocTokenAddr(wallet, mint) {
-    return (await anchor.web3.PublicKey.findProgramAddress([
-        wallet.toBuffer(),
-        tokenProgramKey.toBuffer(),
-        mint.toBuffer(),
-    ], assocTokenProgramKey))[0];
+function findAssocTokenAddr(wallet, mint) {
+    return findAddr([wallet.toBuffer(), tokenProgramKey.toBuffer(), mint.toBuffer()], assocTokenProgramKey);
 }
 
 async function main() {
-    let [controlStateKey] = anchor.utils.publicKey.findProgramAddressSync([Buffer.from("STATE")], controllerKey);
-    let [uxdMintKey] = anchor.utils.publicKey.findProgramAddressSync([Buffer.from("STABLECOIN")], controllerKey);
+    // keys for controller.new
+    let controlStateKey = findAddr([Buffer.from("STATE")], controllerKey);
+    let uxdMintKey = findAddr([Buffer.from("STABLECOIN")], controllerKey);
 
-    let [depositStateKey] = anchor.utils.publicKey.findProgramAddressSync([Buffer.from("STATE")], depositoryKey);
-    let [redeemableMintKey] = anchor.utils.publicKey.findProgramAddressSync([Buffer.from("REDEEMABLE")], depositoryKey);
-    let [depositAccountKey] = anchor.utils.publicKey.findProgramAddressSync([Buffer.from("DEPOSIT")], depositoryKey);
+    // keys for depository.new
+    let depositStateKey = findAddr([Buffer.from("STATE")], depositoryKey);
+    let redeemableMintKey = findAddr([Buffer.from("REDEEMABLE")], depositoryKey);
+    let depositAccountKey = findAddr([Buffer.from("DEPOSIT")], depositoryKey);
+
+    // keys for controller.registerDepository
+    let depositRecordKey = findAddr([Buffer.from("RECORD"), depositoryKey.toBuffer()], controllerKey);
+    let coinPassthroughKey = findAddr([Buffer.from("PASSTHROUGH"), coinMintKey.toBuffer()], controllerKey);
 
     // standard spl associated accounts
-    let walletCoinKey = await findAssocTokenAddr(provider.wallet.publicKey, depositMintKey);
-    let walletRedeemableKey = await findAssocTokenAddr(provider.wallet.publicKey, redeemableMintKey);
+    let userCoinKey = findAssocTokenAddr(provider.wallet.publicKey, coinMintKey);
+    let userRedeemableKey = findAssocTokenAddr(provider.wallet.publicKey, redeemableMintKey);
 
     async function printBalances(skipRed) {
-        let userCoin = (await provider.connection.getTokenAccountBalance(walletCoinKey, TXN_COMMIT))["value"]["uiAmount"];
-        let userRedeemable = skipRed ? 0 : (await provider.connection.getTokenAccountBalance(walletRedeemableKey, TXN_COMMIT))["value"]["uiAmount"];
+        let userCoin = (await provider.connection.getTokenAccountBalance(userCoinKey, TXN_COMMIT))["value"]["uiAmount"];
+        let userRedeemable = skipRed ? 0 : (await provider.connection.getTokenAccountBalance(userRedeemableKey, TXN_COMMIT))["value"]["uiAmount"];
         let programCoin = (await provider.connection.getTokenAccountBalance(depositAccountKey, TXN_COMMIT))["value"]["uiAmount"];
 
         console.log(`* user balance: ${userCoin}\n* user redeemable: ${userRedeemable}\n* program balance: ${programCoin}\n\n`);
@@ -62,7 +69,7 @@ async function main() {
     console.log("payer:", provider.wallet.publicKey.toString());
     console.log("redeemable mint:", redeemableMintKey.toString());
     console.log("program coin:", depositAccountKey.toString());
-    console.log("coin mint:", depositMintKey.toString());
+    console.log("coin mint:", coinMintKey.toString());
     console.log("uxd mint:", uxdMintKey.toString());
     console.log("controller id:", controllerKey.toString());
     console.log("controller state:", controlStateKey.toString());
@@ -71,43 +78,75 @@ async function main() {
     console.log("\n");
 
     // set up the controller
-    await controller.rpc.new({
-        accounts: {
-            authority: provider.wallet.publicKey,
-            state: controlStateKey,
-            uxdMint: uxdMintKey,
-            rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-            systemProgram: anchor.web3.SystemProgram.programId,
-            tokenProgram: tokenProgramKey,
-            program: controllerKey,
-        },
-        signers: [provider.wallet.payer],
-        options: TXN_OPTS,
-    });
+    if(await provider.connection.getAccountInfo(controlStateKey)) {
+        console.log("controller already initialized...");
+    } else {
+        await controller.rpc.new({
+            accounts: {
+                authority: provider.wallet.publicKey,
+                state: controlStateKey,
+                uxdMint: uxdMintKey,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: tokenProgramKey,
+                program: controllerKey,
+            },
+            signers: [provider.wallet.payer],
+            options: TXN_OPTS,
+        });
 
-    console.log("controller initialized!");
+        console.log("controller initialized!");
+    }
 
     // and set up the depository
-    await depository.rpc.new({
-        accounts: {
-            payer: provider.wallet.publicKey,
-            state: depositStateKey,
-            redeemableMint: redeemableMintKey,
-            programCoin: depositAccountKey,
-            coinMint: depositMintKey,
-            rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-            systemProgram: anchor.web3.SystemProgram.programId,
-            tokenProgram: tokenProgramKey,
-            program: depositoryKey,
-        },
-        signers: [provider.wallet.payer],
-        options: TXN_OPTS,
-    });
+    if(await provider.connection.getAccountInfo(depositStateKey)) {
+        console.log("depository already initialized...");
+    } else {
+        await depository.rpc.new(controllerKey, {
+            accounts: {
+                payer: provider.wallet.publicKey,
+                state: depositStateKey,
+                redeemableMint: redeemableMintKey,
+                programCoin: depositAccountKey,
+                coinMint: coinMintKey,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: tokenProgramKey,
+                program: depositoryKey,
+            },
+            signers: [provider.wallet.payer],
+            options: TXN_OPTS,
+        });
 
-    console.log("depository initialized!");
+        console.log("depository initialized!");
+    }
+
+    // aaand register it with the controller
+    if(await provider.connection.getAccountInfo(depositRecordKey)) {
+        console.log("depository already registered...");
+    } else {
+        await controller.rpc.registerDepository(depositoryKey, {
+            accounts: {
+                authority: provider.wallet.publicKey,
+                state: controlStateKey,
+                depositoryRecord: depositRecordKey,
+                depositoryState: depositStateKey,
+                coinMint: coinMintKey,
+                coinPassthrough: coinPassthroughKey,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: tokenProgramKey,
+                program: controllerKey,
+            },
+            signers: [provider.wallet.payer],
+            options: TXN_OPTS,
+        });
+
+        console.log("depository registered!");
+    }
 
     // ok now uhh i just wanna deposit and withdraw. may or may not have to set up the redeemable acct
-    let redeemState = await provider.connection.getAccountInfo(walletRedeemableKey);
+    let redeemState = await provider.connection.getAccountInfo(userRedeemableKey);
 
     // anchor will error if you pass [] or null lol
     var depositIxns = undefined;
@@ -116,7 +155,7 @@ async function main() {
             new anchor.web3.TransactionInstruction({
                 keys: [
                     {pubkey: provider.wallet.publicKey, isSigner: true, isWritable: true},
-                    {pubkey: walletRedeemableKey, isSigner: false, isWritable: true},
+                    {pubkey: userRedeemableKey, isSigner: false, isWritable: true},
                     {pubkey: provider.wallet.publicKey, isSigner: false, isWritable: false},
                     {pubkey: redeemableMintKey, isSigner: false, isWritable: false},
                     {pubkey: anchor.web3.SystemProgram.programId, isSigner: false, isWritable: false},
@@ -138,8 +177,8 @@ async function main() {
             state: depositStateKey,
             programCoin: depositAccountKey,
             redeemableMint: redeemableMintKey,
-            userCoin: walletCoinKey,
-            userRedeemable: walletRedeemableKey,
+            userCoin: userCoinKey,
+            userRedeemable: userRedeemableKey,
             systemProgram: anchor.web3.SystemProgram.programId,
             tokenProgram: tokenProgramKey,
             program: depositoryKey,
@@ -158,8 +197,8 @@ async function main() {
             state: depositStateKey,
             programCoin: depositAccountKey,
             redeemableMint: redeemableMintKey,
-            userCoin: walletCoinKey,
-            userRedeemable: walletRedeemableKey,
+            userCoin: userCoinKey,
+            userRedeemable: userRedeemableKey,
             systemProgram: anchor.web3.SystemProgram.programId,
             tokenProgram: tokenProgramKey,
             program: depositoryKey,
