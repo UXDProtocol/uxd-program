19ad8468aa9fed43a917adcfbcda6aedca908818
Bamboozelino <memail>
2021-09-26 23:04:52 +0800

    acam.sh

---
+++ acam.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd ~/Development/UXD/solana-usds
+
+anchor build --program-name depository
+anchor build --program-name controller
+
+anchor deploy --program-name depository
+anchor deploy --program-name controller
+
+# stupid rust doesnt handle sigpip
+spl-token create-token > /tmp/spl-mint
+COIN_MINT=$(head -1 /tmp/spl-mint | cut -d " " -f 3)
+
+spl-token create-account "$COIN_MINT"
+spl-token mint "$COIN_MINT" 100
+
+node app/index.js "$COIN_MINT"
