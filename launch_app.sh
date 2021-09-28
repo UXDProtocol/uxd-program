9784e9c132f751e17c1973ecf78eb09d2c2e1d46
Bamboozelino <memail>
2021-09-29 01:54:46 +0800

    Comment

--- launch_app.sh
+++ launch_app.sh
@@ -12,15 +12,13 @@ spl-token mint $COIN_MINT 100
 
 export COIN_MINT=$COIN_MINT
 
-
-# # deploy oracle on devnet (TMP HanaHack)
+# # deploy oracle on devnet (TMP HanaHack) -- Actually it's done by ANCHOR itself, genius (look anchor.toml)
 # solana airdrop 1 --url https://api.testnet.solana.com --keypair ~/.config/solana/id.json   
 # anchor deploy --program-name oracle --provider.cluster devnet --provider.wallet ~/.config/solana/devnet.json 
 
 # Run oracle first to have prices
-node app/oracle.js 
-# PID_ORACLE = $!
-#
-# npx mocha -t 50000 app/index.js $COIN_MINT
+npx mocha -t 50000 app/oracle.js 
+
+npx mocha -t 50000 app/index.js $COIN_MINT
 
 # node app/index.js "$COIN_MINT"
