f13192127cb0feeed5aaefe25e8453d9799fceef
Acammm <alexcamill@gmail.com>
2021-12-21 09:28:23 +0100

    Reog intructions for clarity

---
+++ programs/uxd/src/instructions/mango_depository/mod.rs
@@ -0,0 +1,15 @@
+pub mod deposit_insurance_to_mango_depository;
+pub mod mint_with_mango_depository;
+pub mod redeem_from_mango_depository;
+pub mod register_mango_depository;
+pub mod set_mango_depositories_redeemable_soft_cap;
+pub mod set_redeemable_global_supply_cap;
+pub mod withdraw_insurance_from_mango_depository;
+
+pub use deposit_insurance_to_mango_depository::*;
+pub use mint_with_mango_depository::*;
+pub use redeem_from_mango_depository::*;
+pub use register_mango_depository::*;
+pub use set_mango_depositories_redeemable_soft_cap::*;
+pub use set_redeemable_global_supply_cap::*;
+pub use withdraw_insurance_from_mango_depository::*;
