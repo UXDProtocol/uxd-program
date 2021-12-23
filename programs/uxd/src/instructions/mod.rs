31dbd9971a27165b10295d9408435d7da3679f12
Acammm <alexcamill@gmail.com>
2021-12-23 11:16:15 +0100

    Fix missing mango_depository account

--- programs/uxd/src/instructions/mod.rs
+++ programs/uxd/src/instructions/mod.rs
@@ -1,7 +1,7 @@
 pub mod initialize_controller;
-pub mod mango_depository;
+pub mod mango_dex;
 pub mod set_redeemable_global_supply_cap;
 
 pub use initialize_controller::*;
-pub use mango_depository::*;
+pub use mango_dex::*;
 pub use set_redeemable_global_supply_cap::*;
