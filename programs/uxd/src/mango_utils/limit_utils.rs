7825dd3681f66ae149104f2a2c99cf1521027730
Acammm <alexcamill@gmail.com>
2021-12-27 12:23:37 +0100

    move code to perp_account_utils

--- programs/uxd/src/mango_utils/limit_utils.rs
+++ programs/uxd/src/mango_utils/limit_utils.rs
@@ -1,6 +1,3 @@
-use std::ops::Mul;
-use std::ops::Sub;
-
 use super::Order;
 use super::PerpInfo;
 use crate::ErrorCode;
@@ -8,7 +5,6 @@ use crate::UxdResult;
 use crate::SLIPPAGE_BASIS;
 use fixed::types::I80F48;
 use mango::matching::Side;
-use mango::state::PerpAccount;
 
 // Worse execution price for a provided slippage and side
 pub fn limit_price(price: I80F48, slippage: u32, side: Side) -> I80F48 {
@@ -48,26 +44,4 @@ pub fn check_effective_order_price_versus_limit_price(
         }
     };
     Err(ErrorCode::InvalidSlippage)
-}
-
-// Return the base position + the amount that's on EventQueue waiting to be processed
-pub fn total_perp_base_lot_position(perp_account: &PerpAccount) -> i64 {
-    perp_account
-        .base_position
-        .checked_add(perp_account.taker_base)
-        .unwrap()
-}
-
-// Return the quote position + the amount that's on EventQueue waiting to be processed (minus fees)
-pub fn total_perp_quote_position(perp_account: &PerpAccount, perp_info: &PerpInfo) -> i64 {
-    let taker_quote = I80F48::from_num(perp_account.taker_quote)
-        .checked_mul(perp_info.quote_lot_size)
-        .unwrap();
-    let fee_amount = taker_quote.abs().mul(perp_info.taker_fee);
-    let quote_change = taker_quote.sub(fee_amount);
-    let total_quote_position = perp_account
-        .quote_position
-        .checked_add(quote_change)
-        .unwrap();
-    total_quote_position.checked_to_num().unwrap()
 }
