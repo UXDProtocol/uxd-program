62b5da9edff81655a2116dc9b9e30d55678d3a85
Acammm <alexcamill@gmail.com>
2021-12-29 11:43:22 +0100

    Remove old TS tests

--- tests/old/print_balances_wsol_depository.ts
+++
@@ -1,9 +0,0 @@
-import { printUserBalances, printDepositoryInfo } from "../utils";
-import { depositoryWSOL, mango } from "../constants";
-
-describe("==== print balances info ====", () => {
-    it("", async () => {
-        await printUserBalances();
-        await printDepositoryInfo(depositoryWSOL, mango);
-    });
-});
