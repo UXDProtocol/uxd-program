8b032d727242ac7fdb62f38dddfeb4939fc7fdd4
acamill <40299747+acamill@users.noreply.github.com>
2022-01-06 14:01:35 -0800

    Additional computation to offset unsettled state from mango (and remove reliance of `consumeEvents` in the frontend) (#80) (#86)
    * Take into account the unconsumed events directly in the program to get free from consume events
    
    * move code to perp_account_utils
    
    * use dist
    
    * Fix delta on redeemables
    
    * Changes after review from @mshneider

---
+++ programs/uxd/src/mango_utils/perp_account_utils.rs
@@ -0,0 +1,28 @@
+use std::ops::{Mul, Sub};
+
+use fixed::types::I80F48;
+use mango::state::PerpAccount;
+
+use super::PerpInfo;
+
+// Return the base position + the amount that's on EventQueue waiting to be processed
+pub fn total_perp_base_lot_position(perp_account: &PerpAccount) -> i64 {
+    perp_account
+        .base_position
+        .checked_add(perp_account.taker_base)
+        .unwrap()
+}
+
+// Return the quote position + the amount that's on EventQueue waiting to be processed (minus fees)
+pub fn total_perp_quote_position(perp_account: &PerpAccount, perp_info: &PerpInfo) -> i64 {
+    let taker_quote = I80F48::from_num(perp_account.taker_quote)
+        .checked_mul(perp_info.quote_lot_size)
+        .unwrap();
+    let fee_amount = taker_quote.abs().mul(perp_info.taker_fee);
+    let quote_change = taker_quote.sub(fee_amount);
+    let total_quote_position = perp_account
+        .quote_position
+        .checked_add(quote_change)
+        .unwrap();
+    total_quote_position.checked_to_num().unwrap()
+}
