6110de30ed7c1bc72b5bd63225de5ac9ca92da26
acamill <40299747+acamill@users.noreply.github.com>
2022-01-17 14:17:08 +0100

    Integration Tests and CI improvements (#92)
    * Several small improvments
    
    * Test now use container wallet without any ties to other executions. The previous wallet  now became the bank.
    
    * Update devnet id
    
    * feat: moved function validation from lib.rs to each instruction file (#91)
    
    * feat: moved function validation from lib.rs to each instruction file
    
    * cleanup
    
    * Update CI
    
    * Fix sol transfer
    
    * Remove fees from the moved amount
    
    * Fix CI
    
    * Update CI
    
    * CI
    
    * CI
    
    * CI
    
    * ci
    
    * trigger build
    
    * Update CI
    
    * CI
    
    * Remove small values in test that are offsetted by the rent and fee until we implement payer/signer in the mint/redeem
    
    * Fees
    
    * Actually wait for TX to be confirmed before going further to prevent unreliable results
    
    * Move await to the API
    
    * Increase fee offset toprevent issues
    Increase crank interval for devnet
    Cleanup
    
    * - Upgrade to latest UXD Client
    - fix collapsed group
    - fix display in insurance tests
    
    * - fix authority typo
    
    * Test close
    
    * fix lamports
    
    * Fix Cleanup
    
    * Sol not lamports in CI transfer
    
    * Remove small amount tests
    
    * remove unneeded provider
    
    Co-authored-by: Lexington Brill <lexingtonbrill@gmail.com>

--- tests/test_integration_btc.ts
+++ tests/test_integration_btc.ts
@@ -1,10 +1,47 @@
+import { web3 } from "@project-serum/anchor";
+import { Keypair } from "@solana/web3.js";
 import { BTC_DECIMALS, Controller, MangoDepository, USDC_DECIMALS, UXD_DECIMALS } from "@uxdprotocol/uxd-client";
-import { authority, USDC, user, uxdProgramId, BTC } from "./constants";
+import { authority, USDC, bank, uxdProgramId, BTC } from "./constants";
+import { provider } from "./provider";
 import { mangoDepositoryIntegrationSuite } from "./suite/mangoDepositoryIntegrationSuite";
+import { getSolBalance } from "./utils";
 
 const depositoryBTC = new MangoDepository(BTC, "BTC", BTC_DECIMALS, USDC, "USDC", USDC_DECIMALS, uxdProgramId);
 const controllerUXD = new Controller("UXD", UXD_DECIMALS, uxdProgramId);
 
+const user = new Keypair();
+
+console.log("USER =>", user.publicKey.toString());
+
 describe("BTC integration tests", () => {
+    before("Transfer 20 sol from bank to test user", async () => {
+        const transaction = new web3.Transaction().add(
+            web3.SystemProgram.transfer({
+                fromPubkey: bank.publicKey,
+                toPubkey: user.publicKey,
+                lamports: web3.LAMPORTS_PER_SOL * 20
+            }),
+        );
+        await web3.sendAndConfirmTransaction(provider.connection, transaction, [
+            bank,
+        ]);
+    });
+
     mangoDepositoryIntegrationSuite(authority, user, controllerUXD, depositoryBTC);
+
+    // TODO: Add program close
+
+    after("Return remaining balance to the bank", async () => {
+        const userBalance = await getSolBalance(user.publicKey);
+        const transaction = new web3.Transaction().add(
+            web3.SystemProgram.transfer({
+                fromPubkey: user.publicKey,
+                toPubkey: bank.publicKey,
+                lamports: web3.LAMPORTS_PER_SOL * userBalance - 50000
+            }),
+        );
+        await web3.sendAndConfirmTransaction(provider.connection, transaction, [
+            user,
+        ]);
+    });
 });
