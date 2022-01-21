baa0b8953e7d9ca279695daa388321351ccbd9bb
cnek <ctk2012ac@gmail.com>
2022-01-22 02:55:49 +0800

    commented emit

--- programs/uxd/src/instructions/mango_dex/redeem_from_mango_depository.rs
+++ programs/uxd/src/instructions/mango_dex/redeem_from_mango_depository.rs
@@ -245,17 +245,17 @@ pub fn handler(
         order_delta.fee,
     )?;
 
-    emit!(RedeemFromMangoDepositoryEvent {
-        version: ctx.accounts.controller.version,
-        controller: ctx.accounts.controller.key(),
-        depository: ctx.accounts.depository.key(),
-        user: ctx.accounts.user.key(),
-        redeemable_amount,
-        slippage,
-        collateral_delta: order_delta.collateral,
-        redeemable_delta,
-        fee_delta: order_delta.fee,
-    });
+    // emit!(RedeemFromMangoDepositoryEvent {
+    //     version: ctx.accounts.controller.version,
+    //     controller: ctx.accounts.controller.key(),
+    //     depository: ctx.accounts.depository.key(),
+    //     user: ctx.accounts.user.key(),
+    //     redeemable_amount,
+    //     slippage,
+    //     collateral_delta: order_delta.collateral,
+    //     redeemable_delta,
+    //     fee_delta: order_delta.fee,
+    // });
 
     Ok(())
 }
