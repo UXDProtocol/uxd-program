2c8748ca64cbd1b5a42967f9aef2dc1b691c3cd5
Acammm <alexcamill@gmail.com>
2022-02-09 16:42:41 +0800

    Fix migrate test

--- tests/cases/migrateMangoDepositoryToV2Test.ts
+++ tests/cases/migrateMangoDepositoryToV2Test.ts
@@ -7,20 +7,28 @@ import { getProvider, TXN_OPTS } from "../provider";
 export const migrateMangoDepositoryToV2Test = async (authority: Signer, controller: Controller, depository: MangoDepository) => {
     console.group("🧭 migrateMangoDepositoryToV2Test");
     try {
-        // WHEN
-        if ((await uxdHelpers.getMangoDepositoryAccountNoProvider(getProvider().connection, depository, TXN_OPTS)).version != 1) {
-            console.log("🚧 Already migrated.");
-        } else {
-            const txId = await migrateMangoDepositoryToV2(authority, controller, depository);
-            console.log(`🔗 'https://explorer.solana.com/tx/${txId}?cluster=${CLUSTER}'`);
-        }
+        await getProvider().connection.getAccountInfo(controller.pda); // With throw if doesn't exist
+        try {
 
-        // THEN
-        console.log(`🧾 Initialized`, depository.collateralMintSymbol, "Depository");
-        depository.info();
-        console.groupEnd();
+            // WHEN
+            if ((await uxdHelpers.getMangoDepositoryAccountNoProvider(getProvider().connection, depository, TXN_OPTS)).version != 1) {
+                console.log("🚧 Already migrated.");
+            } else {
+                const txId = await migrateMangoDepositoryToV2(authority, controller, depository);
+                console.log(`🔗 'https://explorer.solana.com/tx/${txId}?cluster=${CLUSTER}'`);
+            }
+
+            // THEN
+            console.log(`🧾 Initialized`, depository.collateralMintSymbol, "Depository");
+            depository.info();
+            console.groupEnd();
+        } catch (error) {
+            console.groupEnd();
+            throw error;
+        }
     } catch (error) {
         console.groupEnd();
-        throw error;
+        console.log("🚧 Depository not initialized. Nothing to migrate. All good.");
+        return;
     }
 }
