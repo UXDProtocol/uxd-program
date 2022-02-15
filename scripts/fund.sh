88594fc607442bb72c422c66b4ececc127536a74
Acammm <alexcamill@gmail.com>
2022-02-15 21:45:47 +0800

    Trigger ci

--- scripts/fund.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-AMOUNT_SOL=15
-RECIPIENT_PUBKEY=`solana-keygen pubkey $1`
-
-solana transfer --keypair "./scripts/bank-keypair.json" --commitment processed $RECIPIENT_PUBKEY $AMOUNT_SOL --allow-unfunded-recipient
