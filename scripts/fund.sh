6a5379dc8d8efa480fd8f8b2a87a148f8fdd2e88
Acammm <alexcamill@gmail.com>
2022-02-15 21:03:26 +0800

    Test fund.sh

--- scripts/fund.sh
+++ scripts/fund.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 AMOUNT_SOL=15
+RECIPIENT_PUBKEY=`solana-keygen pubkey $1`
 
-solana transfer --keypair "./bank-keypair.json" --commitment processed $1 $AMOUNT_SOL
+
+solana transfer --keypair "./bank-keypair.json" --commitment processed $RECIPIENT_PUBKEY $AMOUNT_SOL 
