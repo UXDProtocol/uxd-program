35c9e44b49c9894b02f037d030e83d2ce5b927d2
Acammm <alexcamill@gmail.com>
2022-02-16 11:06:12 +0800

    Add prop tests

--- programs/uxd/src/mango_utils/perp_account_utils.rs
+++ programs/uxd/src/mango_utils/perp_account_utils.rs
@@ -17,22 +17,42 @@ pub fn total_perp_base_lot_position(perp_account: &PerpAccount) -> UxdResult<i64
         .ok_or(math_err!())?)
 }
 
-// Return the quote position + the amount that's on EventQueue waiting to be processed (minus fees)
-pub fn total_perp_quote_position(
-    perp_account: &PerpAccount,
-    perp_info: &PerpInfo,
-) -> UxdResult<i64> {
-    let taker_quote = I80F48::from_num(perp_account.taker_quote)
-        .checked_mul(perp_info.quote_lot_size)
-        .ok_or(math_err!())?;
-    let fee_amount = taker_quote
-        .abs()
-        .checked_mul(perp_info.taker_fee)
-        .ok_or(math_err!())?;
-    let quote_change = taker_quote.checked_sub(fee_amount).ok_or(math_err!())?;
-    let total_quote_position = perp_account
-        .quote_position
-        .checked_add(quote_change)
-        .ok_or(math_err!())?;
-    Ok(total_quote_position.checked_to_num().ok_or(math_err!())?)
+// Non regression tests
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use proptest::prelude::*;
+
+    fn mocked_perp_account(taker_base: i64, base_position: i64) -> PerpAccount {
+        PerpAccount {
+            base_position,
+            quote_position: I80F48::ZERO,
+            long_settled_funding: I80F48::ZERO,
+            short_settled_funding: I80F48::ZERO,
+            bids_quantity: 0,
+            asks_quantity: 0,
+            taker_base,
+            taker_quote: 0,
+            mngo_accrued: 0,
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_total_perp_base_lot_position(taker_base in i64::MIN..i64::MAX, base_position in i64::MIN..i64::MAX) {
+            let perp_account = mocked_perp_account(taker_base, base_position);
+            let res = total_perp_base_lot_position(&perp_account);
+
+            match res {
+                Ok(total) => {
+                    println!("{}", total);
+                    prop_assert_eq!(total, taker_base + base_position);
+                }
+                Err(error) => {
+                    prop_assert_eq!(error, UxdError::UxdErrorCode { uxd_error_code: UxdErrorCode::MathError, line: 17, source_file_id: SourceFileId::MangoUtilsPerpAccountUtils });
+                }
+            };
+        }
+    }
 }
