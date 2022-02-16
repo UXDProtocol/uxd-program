20e7fb6ac169135608d853b0a7486d2e1437e4b9
Acammm <alexcamill@gmail.com>
2022-02-16 17:15:47 +0800

    Refactor tests names

--- programs/uxd/src/mango_utils/limit_utils.rs
+++ programs/uxd/src/mango_utils/limit_utils.rs
@@ -66,7 +66,6 @@ pub fn check_effective_order_price_versus_limit_price(
     Err(throw_err!(UxdErrorCode::SlippageReached))
 }
 
-// Non regression tests
 #[cfg(test)]
 mod tests {
 
@@ -97,213 +96,226 @@ mod tests {
         })
     }
 
-    proptest! {
-        /// Tests the price check after placing a Perp order for Minting UXD (Selling Perp to open the Short position)
-        /// combinations with :
-        ///      perp_price between 0$ and 100_000
-        ///      order_price between 0$ and 100_000
-        ///      slippage between 0.01% and 100%
-        #[test]
-        fn test_check_effective_order_price_versus_limit_price_mint(perp_price in 0.0f64..10f64, order_price in 0.0f64..10f64, slippage in 1..SLIPPAGE_BASIS) {
-            // Order.price must be below the perpInfo.price within slippage
-            let side = Side::Bid;
-            let perp_info = mocked_perp_info(perp_price);
-            let order = mocked_order(&perp_info, order_price, side).unwrap();
+    mod check_effective_order_price_versus_limit_price_suite {
+        use super::*;
 
-            let limit_price: f64 = limit_price(I80F48::from_num(perp_price), slippage, side)?.to_num();
-            match check_effective_order_price_versus_limit_price(
-                &perp_info,
-                &order,
-                slippage,
-            ) {
-                Ok(_) => {
-                    prop_assert!(order_price >= limit_price);
-                },
-                Err(error) => {
-                    match error {
-                        UxdError::ProgramError(_) => prop_assert!(false),
-                        UxdError::UxdErrorCode { uxd_error_code, line: _, source_file_id } => {
-                            prop_assert_eq!(source_file_id, SourceFileId::MangoUtilsLimitUtils);
-                            match uxd_error_code {
-                                UxdErrorCode::MathError => prop_assert!(true),
-                                UxdErrorCode::SlippageReached => {
-                                    prop_assert!(order_price <= limit_price);
-                                },
-                                _default => prop_assert!(false)
+        mod mint_suite {
+            use super::*;
+
+            proptest! {
+                /// Tests the price check after placing a Perp order for Minting UXD (Selling Perp to open the Short position)
+                /// combinations with :
+                ///      perp_price between 0$ and 100_000
+                ///      order_price between 0$ and 100_000
+                ///      slippage between 0.01% and 100%
+                #[test]
+                fn proptest(perp_price in 0.0f64..10f64, order_price in 0.0f64..10f64, slippage in 1..SLIPPAGE_BASIS) {
+                    // Order.price must be below the perpInfo.price within slippage
+                    let side = Side::Bid;
+                    let perp_info = mocked_perp_info(perp_price);
+                    let order = mocked_order(&perp_info, order_price, side).unwrap();
+
+                    let limit_price: f64 = limit_price(I80F48::from_num(perp_price), slippage, side)?.to_num();
+                    match check_effective_order_price_versus_limit_price(
+                        &perp_info,
+                        &order,
+                        slippage,
+                    ) {
+                        Ok(_) => {
+                            prop_assert!(order_price >= limit_price);
+                        },
+                        Err(error) => {
+                            match error {
+                                UxdError::ProgramError(_) => prop_assert!(false),
+                                UxdError::UxdErrorCode { uxd_error_code, line: _, source_file_id } => {
+                                    prop_assert_eq!(source_file_id, SourceFileId::MangoUtilsLimitUtils);
+                                    match uxd_error_code {
+                                        UxdErrorCode::MathError => prop_assert!(true),
+                                        UxdErrorCode::SlippageReached => {
+                                            prop_assert!(order_price <= limit_price);
+                                        },
+                                        _default => prop_assert!(false)
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+
+            mod non_regression {
+                use super::*;
+
+                #[test]
+                pub fn test_valid_mint_small_slippage() {
+                    // Order.price must be below the perpInfo.price within slippage
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.09000, Side::Bid).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 1, // 0.1%
+                    );
+                    assert!(ret.is_ok());
+                }
+
+                #[test]
+                pub fn test_valid_mint() {
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.08911, Side::Bid).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 10, // 1%
+                    );
+                    assert!(ret.is_ok());
+                }
+
+                #[test]
+                pub fn test_invalid_mint() {
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.08909, Side::Bid).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 10, // 1%
+                    );
+                    assert!(ret.is_err());
+                }
+            }
         }
-    }
 
-    proptest! {
-        /// Tests the price check after placing a Perp order for Redeeming UXD (Buying Perp to close the outstanding Short position)
-        /// combinations with :
-        ///      perp_price between 0$ and 100_000
-        ///      order_price between 0$ and 100_000
-        ///      slippage between 0.01% and 100%
-        #[test]
-        fn test_check_effective_order_price_versus_limit_price_redeem(perp_price in 0.0f64..10f64, order_price in 0.0f64..10f64, slippage in 1..SLIPPAGE_BASIS) {
-            let side = Side::Ask;
-            let perp_info = mocked_perp_info(perp_price);
-            let order = mocked_order(&perp_info, order_price, side).unwrap();
+        mod redeem_suite {
+            use super::*;
 
-            let limit_price: f64 = limit_price(I80F48::from_num(perp_price), slippage, side)?.to_num();
-            match check_effective_order_price_versus_limit_price(
-                &perp_info,
-                &order,
-                slippage,
-            ) {
-                Ok(_) => {
-                    prop_assert!(order_price <= limit_price);
-                },
-                Err(error) => {
-                    match error {
-                        UxdError::ProgramError(_) => prop_assert!(false),
-                        UxdError::UxdErrorCode { uxd_error_code, line: _, source_file_id } => {
-                            prop_assert_eq!(source_file_id, SourceFileId::MangoUtilsLimitUtils);
-                            match uxd_error_code {
-                                UxdErrorCode::MathError => prop_assert!(true),
-                                UxdErrorCode::SlippageReached => {
-                                    prop_assert!(order_price >= limit_price);
-                                },
-                                _default => prop_assert!(false)
+            proptest! {
+                /// Tests the price check after placing a Perp order for Redeeming UXD (Buying Perp to close the outstanding Short position)
+                /// combinations with :
+                ///      perp_price between 0$ and 100_000
+                ///      order_price between 0$ and 100_000
+                ///      slippage between 0.01% and 100%
+                #[test]
+                fn proptest_redeem(perp_price in 0.0f64..10f64, order_price in 0.0f64..10f64, slippage in 1..SLIPPAGE_BASIS) {
+                    let side = Side::Ask;
+                    let perp_info = mocked_perp_info(perp_price);
+                    let order = mocked_order(&perp_info, order_price, side).unwrap();
+
+                    let limit_price: f64 = limit_price(I80F48::from_num(perp_price), slippage, side)?.to_num();
+                    match check_effective_order_price_versus_limit_price(
+                        &perp_info,
+                        &order,
+                        slippage,
+                    ) {
+                        Ok(_) => {
+                            prop_assert!(order_price <= limit_price);
+                        },
+                        Err(error) => {
+                            match error {
+                                UxdError::ProgramError(_) => prop_assert!(false),
+                                UxdError::UxdErrorCode { uxd_error_code, line: _, source_file_id } => {
+                                    prop_assert_eq!(source_file_id, SourceFileId::MangoUtilsLimitUtils);
+                                    match uxd_error_code {
+                                        UxdErrorCode::MathError => prop_assert!(true),
+                                        UxdErrorCode::SlippageReached => {
+                                            prop_assert!(order_price >= limit_price);
+                                        },
+                                        _default => prop_assert!(false)
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+
+            mod non_regression {
+                use super::*;
+
+                #[test]
+                pub fn test_valid_redeem_small_slippage() {
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.09000, Side::Ask).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 1, // 0.1%
+                    );
+                    assert!(ret.is_ok());
+                }
+
+                #[test]
+                pub fn test_valid_redeem() {
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.09089, Side::Ask).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 10, // 1%
+                    );
+                    assert!(ret.is_ok());
+                }
+
+                #[test]
+                pub fn test_invalid_redeem() {
+                    let perp_info = mocked_perp_info(0.09000);
+                    let order = mocked_order(&perp_info, 0.09091, Side::Ask).unwrap();
+                    let ret = check_effective_order_price_versus_limit_price(
+                        &perp_info, &order, 10, // 1%
+                    );
+                    assert!(ret.is_err());
+                }
+            }
         }
     }
 
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_mint_valid_small_slippage() {
-        // Order.price must be below the perpInfo.price within slippage
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.09000, Side::Bid).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 1, // 0.1%
-        );
-        assert!(ret.is_ok());
-    }
+    mod unit_tests {
+        use super::*;
 
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_mint_valid() {
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.08911, Side::Bid).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 10, // 1%
-        );
-        assert!(ret.is_ok());
-    }
-
-    // REDEEM
-
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_redeem_valid_small_slippage() {
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.09000, Side::Ask).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 1, // 0.1%
-        );
-        assert!(ret.is_ok());
-    }
-
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_mint_invalid() {
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.08909, Side::Bid).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 10, // 1%
-        );
-        assert!(ret.is_err());
-    }
-
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_redeem_valid() {
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.09089, Side::Ask).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 10, // 1%
-        );
-        assert!(ret.is_ok());
-    }
-
-    #[test]
-    pub fn test_check_effective_order_price_versus_limit_price_redeem_invalid() {
-        let perp_info = mocked_perp_info(0.09000);
-        let order = mocked_order(&perp_info, 0.09091, Side::Ask).unwrap();
-        let ret = check_effective_order_price_versus_limit_price(
-            &perp_info, &order, 10, // 1%
-        );
-        assert!(ret.is_err());
-    }
-}
-
-// Unit tests
-#[cfg(test)]
-mod test {
-    use crate::mango_utils::{limit_price, limit_utils::calculate_slippage_amount};
-    use fixed::types::I80F48;
-    use mango::matching::Side;
-    use proptest::prelude::*;
-
-    #[test]
-    fn test_cal_slippage_amount() {
-        // general param
-        let lamport_basis = I80F48::from_num(10u32.pow(9));
-
-        // given price is 24
-        let ui_price = I80F48::from_num(24);
-        let price = ui_price.checked_mul(lamport_basis).unwrap();
-
-        // given slippage is 10%
-        let slippage = 100u32;
-
-        // expected slippage amount
-        let expected = I80F48::from_num(2400000000u64);
-
-        assert_eq!(
-            calculate_slippage_amount(price, slippage)
-                .unwrap()
-                .overflowing_round(),
-            (expected, false)
-        );
-    }
-
-    proptest! {
         #[test]
-        fn test_limit_price_bid(price in 0..1000000000000i128, slippage in 0..u32::MAX) {
-            // create random price in lamport range from 0 to 1000 equivalent uiAmount
-            let fractional_price = I80F48::checked_from_num(price).unwrap();
-            // println!("fractional_price = {}, slippage = {}", fractional_price, slippage);
+        fn test_cal_slippage_amount() {
+            // general param
+            let lamport_basis = I80F48::from_num(10u32.pow(9));
 
-            let limit_price = limit_price(fractional_price, slippage, Side::Bid).unwrap();
+            // given price is 24
+            let ui_price = I80F48::from_num(24);
+            let price = ui_price.checked_mul(lamport_basis).unwrap();
 
-            let slippage_amount = calculate_slippage_amount(fractional_price, slippage).unwrap();
-            // expected limit price
-            let price_sub_slippage = fractional_price.checked_sub(slippage_amount).unwrap();
+            // given slippage is 10%
+            let slippage = 100u32;
 
-            prop_assert_eq!(limit_price, price_sub_slippage);
+            // expected slippage amount
+            let expected = I80F48::from_num(2400000000u64);
+
+            assert_eq!(
+                calculate_slippage_amount(price, slippage)
+                    .unwrap()
+                    .overflowing_round(),
+                (expected, false)
+            );
         }
-    }
 
-    proptest! {
-        #[test]
-        fn test_limit_price_ask(price in 0..1000000000000i128, slippage in 0..u32::MAX) {
-            // create random price in lamport range from 0 to 1000 equivalent uiAmount
-            let fractional_price = I80F48::checked_from_num(price).unwrap();
-            // println!("fractional_price = {}, slippage = {}", fractional_price, slippage);
+        proptest! {
+            #[test]
+            fn test_limit_price_bid(price in 0..1000000000000i128, slippage in 0..u32::MAX) {
+                // create random price in lamport range from 0 to 1000 equivalent uiAmount
+                let fractional_price = I80F48::checked_from_num(price).unwrap();
+                // println!("fractional_price = {}, slippage = {}", fractional_price, slippage);
 
-            let limit_price = limit_price(fractional_price, slippage, Side::Ask).unwrap();
+                let limit_price = limit_price(fractional_price, slippage, Side::Bid).unwrap();
 
-            let slippage_amount = calculate_slippage_amount(fractional_price, slippage).unwrap();
-            // expected limit price
-            let price_add_slippage = fractional_price.checked_add(slippage_amount).unwrap();
+                let slippage_amount = calculate_slippage_amount(fractional_price, slippage).unwrap();
+                // expected limit price
+                let price_sub_slippage = fractional_price.checked_sub(slippage_amount).unwrap();
 
-            prop_assert_eq!(limit_price, price_add_slippage);
+                prop_assert_eq!(limit_price, price_sub_slippage);
+            }
+        }
+
+        proptest! {
+            #[test]
+            fn test_limit_price_ask(price in 0..1000000000000i128, slippage in 0..u32::MAX) {
+                // create random price in lamport range from 0 to 1000 equivalent uiAmount
+                let fractional_price = I80F48::checked_from_num(price).unwrap();
+                // println!("fractional_price = {}, slippage = {}", fractional_price, slippage);
+
+                let limit_price = limit_price(fractional_price, slippage, Side::Ask).unwrap();
+
+                let slippage_amount = calculate_slippage_amount(fractional_price, slippage).unwrap();
+                // expected limit price
+                let price_add_slippage = fractional_price.checked_add(slippage_amount).unwrap();
+
+                prop_assert_eq!(limit_price, price_add_slippage);
+            }
         }
     }
 }
