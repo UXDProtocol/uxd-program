d29d01daf11d0e21fca59aa9793723c692cf3052
Acammm <alexcamill@gmail.com>
2022-02-16 20:14:19 +0800

    Add some clippy stuff

--- programs/uxd/src/instructions/set_mango_depositories_redeemable_soft_cap.rs
+++ programs/uxd/src/instructions/set_mango_depositories_redeemable_soft_cap.rs
@@ -44,6 +44,7 @@ pub fn handler(
 impl<'info> SetMangoDepositoriesRedeemableSoftCap<'info> {
     // Asserts that the Mango Depositories redeemable soft cap is between 0 and MAX_REDEEMABLE_GLOBAL_SUPPLY_CAP.
     pub fn validate(&self, redeemable_soft_cap: u64) -> ProgramResult {
+        #[deny(clippy::absurd_extreme_comparisons)]
         check!(
             redeemable_soft_cap <= MAX_MANGO_DEPOSITORIES_REDEEMABLE_SOFT_CAP,
             UxdErrorCode::InvalidMangoDepositoriesRedeemableSoftCap
