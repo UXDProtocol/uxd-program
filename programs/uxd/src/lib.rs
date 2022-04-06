464448c73e354b537873f98356fce951ba81e10e
Max Planck <maxplanck.crypto@gmail.com>
2022-04-06 14:38:52 -0500

    first

--- programs/uxd/src/lib.rs
+++ programs/uxd/src/lib.rs
@@ -16,7 +16,7 @@ pub mod test;
 // CI Uses F3UToS4WKQkyAAs5TwM_21ANq2xNfDRB7tGRWx4DxapaR on Devnet
 // (it's auto swapped by the script, keypair are held in target/deployment)
 #[cfg(feature = "development")]
-solana_program::declare_id!("AYJ5nxpFp92arm9HZaAcLzuHwbtmmEsrxw8qB7W29YF3");
+solana_program::declare_id!("EXQDPFMw1JQjMzqXJDZccpLTBWHKBwkMiG2vwRvMHe6K");
 #[cfg(feature = "production")]
 solana_program::declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
 
