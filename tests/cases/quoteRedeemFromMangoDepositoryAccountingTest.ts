b86fa13f80e2a9f45e2020d430a75b8bcfdfd591
Max Planck <maxplanck.crypto@gmail.com>
2022-04-20 00:30:15 -0500

    nothing

--- tests/cases/quoteRedeemFromMangoDepositoryAccountingTest.ts
+++ tests/cases/quoteRedeemFromMangoDepositoryAccountingTest.ts
@@ -15,17 +15,6 @@ export const quoteRedeemFromMangoDepositoryAccountingTest = async function(redee
         const options = TXN_OPTS;
 
         // GIVEN
-        const userQuoteATA: PublicKey = await utils.token.associatedAddress({
-            mint: depository.quoteMint,
-            owner: user.publicKey,
-        });
-        const userRedeemableATA: PublicKey = await utils.token.associatedAddress({
-            mint: controller.redeemableMintPda,
-            owner: user.publicKey,
-        });
-        const userQuoteBalance = await getBalance(userQuoteATA);
-        const userRedeemableBalance = await getBalance(userRedeemableATA);
-
         const depositoryAccount = await depository.getOnchainAccount(connection, options);
         const depositoryRedeemable = nativeToUi(depositoryAccount.redeemableAmountUnderManagement.toNumber(), depository.quoteMintDecimals);
         const controllerAccount = await controller.getOnchainAccount(connection, options);
@@ -36,28 +25,17 @@ export const quoteRedeemFromMangoDepositoryAccountingTest = async function(redee
         console.log(`🔗 'https://explorer.solana.com/tx/${txId}?cluster=${CLUSTER}'`);
 
         // THEN
-        const userQuoteATA_post: PublicKey = await utils.token.associatedAddress({
-            mint: depository.quoteMint,
-            owner: user.publicKey,
-        });
-        const userRedeemableATA_post: PublicKey = await utils.token.associatedAddress({
-            mint: controller.redeemableMintPda,
-            owner: user.publicKey,
-        });
-        const userQuoteBalance_post = await getBalance(userQuoteATA_post);
-        const userRedeemableBalance_post = await getBalance(userRedeemableATA_post);
-
         const depositoryAccount_post = await depository.getOnchainAccount(connection, options);
         const depositoryRedeemable_post = nativeToUi(depositoryAccount_post.redeemableAmountUnderManagement.toNumber(), depository.quoteMintDecimals);
         const controllerAccount_post = await controller.getOnchainAccount(connection, options);
         const controllerRedeemable_post = nativeToUi(controllerAccount_post.redeemableCirculatingSupply.toNumber(), controller.redeemableMintDecimals);
     
         // Accounting tests
-
+        // Check depository and controller balances
         console.groupEnd();
     } catch (error) {
         console.error("❌", error);
         console.groupEnd();
         throw error;
     }
-} 
+}
