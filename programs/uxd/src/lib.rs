457632fa53f8d3ad625bfb7a366e0c0974aa326c
Max Planck <maxplanck.crypto@gmail.com>
2022-05-10 20:21:17 -0500

    committed

--- programs/uxd/src/lib.rs
+++ programs/uxd/src/lib.rs
@@ -15,7 +15,7 @@ pub mod test;
 // CI Uses F3UToS4WKQkyAAs5TwM_21ANq2xNfDRB7tGRWx4DxapaR on Devnet
 // (it's auto swapped by the script, keypair are held in target/deployment)
 #[cfg(feature = "development")]
-solana_program::declare_id!("5JxVzC25JQWuNr5jn2yZXSzB35bRyYgqCi39GGzxs7n4");
+solana_program::declare_id!("Hxe5F6biJ9a9fBqqu9dWFpJaPprFk9y2fJykVEurzfHV");
 #[cfg(feature = "production")]
 solana_program::declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
 
