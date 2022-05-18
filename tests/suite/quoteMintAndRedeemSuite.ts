212103a722b5a6e3c014f148f5f965b5f8185d4e
Bamboozelino <memail>
2022-05-18 12:21:56 +0800

    Fix quote mint redeem

--- tests/suite/quoteMintAndRedeemSuite.ts
+++ tests/suite/quoteMintAndRedeemSuite.ts
@@ -1,64 +1,84 @@
+import { NATIVE_MINT } from "@solana/spl-token";
 import { Signer } from "@solana/web3.js";
 import { Controller, MangoDepository, PnLPolarity } from "@uxd-protocol/uxd-client";
+import { mintWithMangoDepositoryTest } from "../cases/mintWithMangoDepositoryTest";
 import { quoteMintWithMangoDepositoryAccountingTest } from "../cases/quoteMintWithMangoDepositoryAccountingTest";
 import { quoteMintWithMangoDepositoryTest } from "../cases/quoteMintWithMangoDepositoryTest";
 import { quoteRedeemFromMangoDepositoryAccountingTest } from "../cases/quoteRedeemFromMangoDepositoryAccountingTest";
 import { quoteRedeemFromMangoDepositoryTest } from "../cases/quoteRedeemFromMangoDepositoryTest";
 import { setMangoDepositoryQuoteMintAndRedeemFeeTest } from "../cases/setMangoDepositoryQuoteMintAndRedeemFeeTest";
 import { TXN_OPTS } from "../connection";
+import { slippageBase } from "../constants";
 import { mango } from "../fixtures";
+import { transferSol, transferTokens } from "../utils";
 
 
 export const quoteMintAndRedeemSuite = function (authority: Signer, user: Signer, payer: Signer, controller: Controller, depository: MangoDepository) {
-    // it(`Ensure user has UXD`, async function () {
-    //     await mintWithMangoDepositoryTest(0.01, 500, user, controller, depository, mango);
-    // });
+
+    before(`Transfer 5,000${depository.quoteMintSymbol} from payer to user`, async function () {
+        await transferTokens(5000, depository.quoteMint, depository.quoteMintDecimals, payer, user.publicKey);
+    });
+
+    before(`Transfer 5,000 USD worth of ${depository.collateralMintSymbol} from payer to user`, async function () {
+        const perpPrice = await depository.getCollateralPerpPriceUI(mango);
+        const amount = 5_000 / perpPrice;
+        console.log("[🧾 amount", amount, depository.collateralMintSymbol, "]");
+        // For Wsol we send sol, the API handle the wrapping before each minting
+        if (depository.collateralMint.equals(NATIVE_MINT)) {
+            await transferSol(amount, payer, user.publicKey);
+        } else {
+            await transferTokens(amount, depository.collateralMint, depository.collateralMintDecimals, payer, user.publicKey);
+        }
+    });
+
+    before(`Mint 3000 ${controller.redeemableMintSymbol} (${20 / slippageBase * 100} % slippage)`, async function () {
+        const perpPrice = await depository.getCollateralPerpPriceUI(mango);
+        const amount = 3000 / perpPrice;
+        console.log("[🧾 amount", amount, depository.collateralMintSymbol, "]");
+        await mintWithMangoDepositoryTest(amount, 20, user, controller, depository, mango, payer);
+    });
 
     it(`Change the quote mint and redeem fees to 0`, async function () {
         await setMangoDepositoryQuoteMintAndRedeemFeeTest(0, authority, controller, depository);
     });
 
-    it(`Quote mint or redeem a small amount (without fees)`, async function () {
+    it(`Quote mint or redeem 10$ (without fees)`, async function () {
 
         const offsetUnrealizedPnl = await depository.getOffsetUnrealizedPnl(mango, TXN_OPTS);
         const polarity = offsetUnrealizedPnl > 0 ? PnLPolarity.Positive : PnLPolarity.Negative;
 
-        if (Math.abs(offsetUnrealizedPnl) < 1) {
-            console.log("🔵  skipping rebalancing, unrealized pnl too small");
+        if (Math.abs(offsetUnrealizedPnl) < 10) {
+            console.log("🔵  skipping mint/redeem, unrealized pnl too small");
             return;
         }
         switch (polarity) {
             case `Positive`: {
-                console.log("Quote Redeem!");
-                await quoteRedeemFromMangoDepositoryTest(1, user, controller, depository, mango, payer);
+                await quoteRedeemFromMangoDepositoryTest(10, user, controller, depository, mango, payer);
                 break;
             }
             case `Negative`: {
-                console.log("Quote Mint!");
-                await quoteMintWithMangoDepositoryTest(1, user, controller, depository, mango, payer);
+                await quoteMintWithMangoDepositoryTest(10, user, controller, depository, mango, payer);
                 break;
             }
         }
     });
 
-    it(`Accounting test for quote mint or redeem a small amount (without fees)`, async function () {
+    it(`Accounting test for quote mint or redeem 10$ (without fees)`, async function () {
 
         const offsetUnrealizedPnl = await depository.getOffsetUnrealizedPnl(mango, TXN_OPTS);
         const polarity = offsetUnrealizedPnl > 0 ? PnLPolarity.Positive : PnLPolarity.Negative;
 
-        if (Math.abs(offsetUnrealizedPnl) < 1) {
-            console.log("🔵  skipping rebalancing, unrealized pnl too small");
+        if (Math.abs(offsetUnrealizedPnl) < 10) {
+            console.log("🔵  skipping quote mint/redeem, unrealized pnl too small");
             return;
         }
         switch (polarity) {
             case `Positive`: {
-                console.log("Quote Redeem Accounting!");
-                await quoteRedeemFromMangoDepositoryAccountingTest(1, user, controller, depository, mango, payer);
+                await quoteRedeemFromMangoDepositoryAccountingTest(10, user, controller, depository, mango, payer);
                 break;
             }
             case `Negative`: {
-                console.log("Quote Mint Accounting!");
-                await quoteMintWithMangoDepositoryAccountingTest(1, user, controller, depository, mango, payer);
+                await quoteMintWithMangoDepositoryAccountingTest(10, user, controller, depository, mango, payer);
                 break;
             }
         }
@@ -68,73 +88,66 @@ export const quoteMintAndRedeemSuite = function (authority: Signer, user: Signer
         await setMangoDepositoryQuoteMintAndRedeemFeeTest(5, authority, controller, depository);
     });
 
-    it(`Quote mint or redeem a small amount (with fees)`, async function () {
+    it(`Quote mint or redeem 10$ (with fees)`, async function () {
+
         const offsetUnrealizedPnl = await depository.getOffsetUnrealizedPnl(mango, TXN_OPTS);
         const polarity = offsetUnrealizedPnl > 0 ? PnLPolarity.Positive : PnLPolarity.Negative;
 
-        if (Math.abs(offsetUnrealizedPnl) < 1) {
-            console.log("🔵  skipping rebalancing, unrealized pnl too small");
+        if (Math.abs(offsetUnrealizedPnl) < 10) {
+            console.log("🔵  skipping mint/redeem, unrealized pnl too small");
             return;
         }
         switch (polarity) {
             case `Positive`: {
-                console.log("Quote Redeem!");
-                await quoteRedeemFromMangoDepositoryTest(1, user, controller, depository, mango, payer);
+                await quoteRedeemFromMangoDepositoryTest(10, user, controller, depository, mango, payer);
                 break;
             }
             case `Negative`: {
-                console.log("Quote Mint!");
-                await quoteMintWithMangoDepositoryTest(1, user, controller, depository, mango, payer);
+                await quoteMintWithMangoDepositoryTest(10, user, controller, depository, mango, payer);
                 break;
             }
         }
     });
 
-    it(`Accounting test for quote mint or redeem a small amount (with fees)`, async function () {
+    it(`Accounting test for quote mint or redeem 10$ (with fees)`, async function () {
+
         const offsetUnrealizedPnl = await depository.getOffsetUnrealizedPnl(mango, TXN_OPTS);
         const polarity = offsetUnrealizedPnl > 0 ? PnLPolarity.Positive : PnLPolarity.Negative;
 
-        if (Math.abs(offsetUnrealizedPnl) < 1) {
-            console.log("🔵  skipping rebalancing, unrealized pnl too small");
+        if (Math.abs(offsetUnrealizedPnl) < 10) {
+            console.log("🔵  skipping quote mint/redeem, unrealized pnl too small");
             return;
         }
         switch (polarity) {
             case `Positive`: {
-                console.log("Quote Redeem Accounting!");
-                await quoteRedeemFromMangoDepositoryAccountingTest(1, user, controller, depository, mango, payer);
+                await quoteRedeemFromMangoDepositoryAccountingTest(10, user, controller, depository, mango, payer);
                 break;
             }
             case `Negative`: {
-                console.log("Quote Mint Accounting!");
-                await quoteMintWithMangoDepositoryAccountingTest(1, user, controller, depository, mango, payer);
+                await quoteMintWithMangoDepositoryAccountingTest(10, user, controller, depository, mango, payer);
                 break;
             }
         }
     });
 
-    it(`Quote mint or redeem remaining amount (with fees)`, async function () {
+    it(`Quote mint or redeem 1000$ (with fees)`, async function () {
+
         const offsetUnrealizedPnl = await depository.getOffsetUnrealizedPnl(mango, TXN_OPTS);
         const polarity = offsetUnrealizedPnl > 0 ? PnLPolarity.Positive : PnLPolarity.Negative;
 
-        // Else the collateral price might change and fail the test
-        const amount = offsetUnrealizedPnl * 0.9;
-
-        if (Math.abs(offsetUnrealizedPnl) < 1) {
-            console.log("🔵  skipping rebalancing, unrealized pnl too small");
+        if (Math.abs(offsetUnrealizedPnl) < 1000) {
+            console.log("🔵  skipping mint/redeem, unrealized pnl too small");
             return;
         }
         switch (polarity) {
             case `Positive`: {
-                console.log("Quote Redeem! Amount:", amount);
-                await quoteRedeemFromMangoDepositoryTest(amount, user, controller, depository, mango, payer);
+                await quoteRedeemFromMangoDepositoryTest(1000, user, controller, depository, mango, payer);
                 break;
             }
             case `Negative`: {
-                console.log("Quote Mint! Amount:", amount);
-                await quoteMintWithMangoDepositoryTest(amount, user, controller, depository, mango, payer);
+                await quoteMintWithMangoDepositoryTest(1000, user, controller, depository, mango, payer);
                 break;
             }
         }
     });
-
 }
