5a355d2ef74dd5c1001e827718902552bd466ac3
Max Planck <maxplanck.crypto@gmail.com>
2022-06-08 21:54:37 -0500

    added test coverage

--- programs/uxd/src/lib.rs
+++ programs/uxd/src/lib.rs
@@ -15,7 +15,7 @@ pub mod test;
 // CI Uses F3UToS4WKQkyAAs5TwM_21ANq2xNfDRB7tGRWx4DxapaR on Devnet
 // (it's auto swapped by the script, keypair are held in target/deployment)
 #[cfg(feature = "development")]
-solana_program::declare_id!("6k1FocL2LrevETpyRqdFf5hhtudT6bAL9GxedFe951gH");
+solana_program::declare_id!("9Sq2Jf2NRF7797uxozUvNpmsZSCssXt3zWcrgFbUNC4t");
 #[cfg(feature = "production")]
 solana_program::declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
 
