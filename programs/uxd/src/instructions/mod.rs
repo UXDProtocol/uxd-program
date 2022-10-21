44ee3938ae9cecaf9a7c658b16f3210b0cf29a53
Grégory NEUT <gregory.neut@gmail.com>
2022-10-21 11:23:47 +0400

    Set proper dependencies

--- programs/uxd/src/instructions/mod.rs
+++ programs/uxd/src/instructions/mod.rs
@@ -2,10 +2,12 @@ pub mod edit_controller;
 pub mod edit_mango_depository;
 pub mod initialize_controller;
 pub mod mango_dex;
+pub mod mango_reimburse;
 pub mod register_mango_depository;
 
 pub use edit_controller::*;
 pub use edit_mango_depository::*;
 pub use initialize_controller::*;
 pub use mango_dex::*;
+pub use mango_reimburse::*;
 pub use register_mango_depository::*;
