6150b7cc03d6ebd48881979c69e60d8e35562d5b
cnek <ctk2012ac@gmail.com>
2022-11-05 09:23:37 +0900

    test

--- tests/cases/reinjectMangoToIdentityDepositoryTest.ts
+++ tests/cases/reinjectMangoToIdentityDepositoryTest.ts
@@ -1,51 +1,90 @@
-import { Signer, PublicKey } from "@solana/web3.js";
+import { Signer } from "@solana/web3.js";
 import { IdentityDepository } from "@uxd-protocol/uxd-client";
 import { Controller, MangoDepository, findATAAddrSync, nativeToUi } from "@uxd-protocol/uxd-client";
 import { expect } from "chai";
 import { reinjectMangoToIdentityDepository } from "../api";
+import { getConnection, TXN_OPTS } from "../connection";
 import { CLUSTER } from "../constants";
 import { getBalance } from "../utils";
 
-export const mangoReimburseTest = async function (
+export const reinjectMangoToIdentityDepositoryTest = async function (
   user: Signer,
   payer: Signer,
   controller: Controller,
   depository: IdentityDepository,
   mangoDepository: MangoDepository
 ): Promise<void> {
-  console.group("🧭 mangoReimburseTest");
+  console.group("🧭 reinjectMangoToIdentityDepository");
+
   try {
     // GIVEN
-    const [authorityTokenAccount] = findATAAddrSync(authority.publicKey, tokenMint);
-    const [depositoryTokenAccount] = findATAAddrSync(depository.pda, tokenMint);
+    const [userCollateralATA] = findATAAddrSync(user.publicKey, depository.collateralMint);
 
-    const [authorityTokenAccountBalance_pre, depositoryTokenAccountBalance_pre] = await Promise.all([
-      getBalance(authorityTokenAccount),
-      getBalance(depositoryTokenAccount),
-    ]);
+    const [userCollateralBalance_pre, collateralVaultBalance_pre, onChainDepository_pre, onChainMangoDepository] =
+      await Promise.all([
+        getBalance(userCollateralATA),
+        getBalance(depository.collateralVaultPda),
+        depository.getOnchainAccount(getConnection(), TXN_OPTS),
+        mangoDepository.getOnchainAccount(getConnection(), TXN_OPTS),
+      ]);
 
     // WHEN
     const txId = await reinjectMangoToIdentityDepository(user, payer, controller, depository, mangoDepository);
     console.log(`🔗 'https://explorer.solana.com/tx/${txId}?cluster=${CLUSTER}'`);
 
     // THEN
-    const [authorityTokenAccountBalance_post, depositoryTokenAccountBalance_post] = await Promise.all([
-      getBalance(authorityTokenAccount),
-      getBalance(depositoryTokenAccount),
+    const [userCollateralBalance_post, collateralVaultBalance_post, onChainDepository_post] = await Promise.all([
+      getBalance(userCollateralATA),
+      getBalance(depository.collateralVaultPda),
+      depository.getOnchainAccount(getConnection(), TXN_OPTS),
     ]);
 
-    const receivedTokenAmount = authorityTokenAccountBalance_post - authorityTokenAccountBalance_pre;
-    const uiReceivedTokenAmount = nativeToUi(receivedTokenAmount, tokenDecimals);
+    // Checked user collateral used
+    const userCollateralDelta = Number(
+      (userCollateralBalance_pre - userCollateralBalance_post).toFixed(depository.collateralMintDecimals)
+    );
 
-    console.log(`🧾 Received`, uiReceivedTokenAmount.toLocaleString(), tokenMintSymbol);
-    expect(receivedTokenAmount.toString()).equals(
-      expectedReceivedTokenAmount.toString(),
-      "The amount of received tokens should be bigger than 0"
+    // Checked identity depo's collateral vault increment
+    const collateralVaultDelta = Number(
+      (collateralVaultBalance_post - collateralVaultBalance_pre).toFixed(depository.collateralMintDecimals)
     );
-    expect((depositoryTokenAccountBalance_post - depositoryTokenAccountBalance_pre).toString()).equals(
-      0,
-      "Depository ATA balance should not change"
+
+    // Checked identity depo's accounting of collateral deposited
+    const collateralAmountDepositedDelta = nativeToUi(
+      Number(
+        (onChainDepository_post.collateralAmountDeposited - onChainDepository_pre.collateralAmountDeposited).toFixed(
+          depository.collateralMintDecimals
+        )
+      ),
+      depository.collateralMintDecimals
     );
+
+    // Checked identity depo's accounting of collateral deposited
+    const redeemableAmountUnderManagementDelta = nativeToUi(
+      Number(
+        (
+          onChainDepository_post.redeemableAmountUnderManagement - onChainDepository_pre.redeemableAmountUnderManagement
+        ).toFixed(depository.collateralMintDecimals)
+      ),
+      depository.collateralMintDecimals
+    );
+
+    // Expected reinjected amount should equal redeemable under this mango depo
+    const expectedReinjectedAmount = Number(
+      nativeToUi(onChainMangoDepository.redeemableAmountUnderManagement, controller.redeemableMintDecimals)
+    );
+
+    console.log(
+      `🧾 Reinjected`,
+      userCollateralDelta.toLocaleString(),
+      depository.collateralMintSymbol,
+      "to identity depository"
+    );
+
+    expect(userCollateralDelta).equals(expectedReinjectedAmount);
+    expect(collateralVaultDelta).equals(expectedReinjectedAmount);
+    expect(collateralAmountDepositedDelta).equals(expectedReinjectedAmount);
+    expect(redeemableAmountUnderManagementDelta).equals(expectedReinjectedAmount);
 
     console.groupEnd();
   } catch (error) {
