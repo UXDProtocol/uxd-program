ef9aa7a5237e18bb6a185de6ccaf0c839aeeaa08
cnek <ctk2012ac@gmail.com>
2022-12-29 17:57:07 +0800

    fixed prod error

--- programs/uxd/src/utils/validate_collateral_mint_usdc.rs
+++ programs/uxd/src/utils/validate_collateral_mint_usdc.rs
@@ -1,6 +1,7 @@
 use crate::{error::UxdError, state::controller::Controller};
 use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
+use std::str::FromStr;
 
 pub fn validate_collateral_mint_usdc(
     collateral_mint: &Account<Mint>,
@@ -11,9 +12,9 @@ pub fn validate_collateral_mint_usdc(
     #[cfg(feature = "production")]
     {
         let usdc_mint: Pubkey =
-            Pubkey::from_str(b"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
+            Pubkey::from_str("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
         require!(
-            self.collateral_mint.key().eq(&usdc_mint),
+            collateral_mint.key().eq(&usdc_mint),
             UxdError::CollateralMintNotAllowed,
         );
     }
