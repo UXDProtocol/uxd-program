4f4c4573481cced9418a440aa2089760fe761df7
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-02-06 13:34:05 +0900

    simplify-temporarily

--- programs/uxd/src/lib.rs
+++ programs/uxd/src/lib.rs
@@ -12,11 +12,6 @@ pub mod mercurial_utils;
 pub mod state;
 pub mod utils;
 
-// CI Uses F3UToS4WKQkyAAs5TwM_21ANq2xNfDRB7tGRWx4DxapaR on Devnet
-// (it's auto swapped by the script, keypair are held in target/deployment)
-#[cfg(feature = "development")]
-declare_id!("EqMrasrCXsbE6mpXaopUH1qip7auwX4AF5UWWpXaLr6u");
-#[cfg(feature = "production")]
 declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
 
 // Version used for accounts structure and future migrations
