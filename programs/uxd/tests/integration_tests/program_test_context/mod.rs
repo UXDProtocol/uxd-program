193c186ff3a4059855f84bf37a32047145fc24b3
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-02-07 12:37:39 +0900

    up

--- programs/uxd/tests/integration_tests/program_test_context/mod.rs
+++ programs/uxd/tests/integration_tests/program_test_context/mod.rs
@@ -1,9 +1,7 @@
-pub mod program_test_add_mint;
 pub mod program_test_context_execute_instruction_with_signer;
 pub mod program_test_context_lamports_airdrop;
 pub mod program_test_context_lamports_transfer;
 
-pub use program_test_add_mint::*;
 pub use program_test_context_execute_instruction_with_signer::*;
 pub use program_test_context_lamports_airdrop::*;
 pub use program_test_context_lamports_transfer::*;
