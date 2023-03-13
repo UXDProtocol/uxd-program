e042947f840971cd2d40925c3a638c94fbfd3128
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-03-13 21:16:17 +0900

    renamed

--- programs/uxd/tests/unit_tests/utils/math/compute_value_for_shares_amount_floor.rs
+++ programs/uxd/tests/unit_tests/utils/math/compute_value_for_shares_amount_floor.rs
@@ -3,40 +3,40 @@
 mod test_compute_value_for_shares_amount {
     use anchor_lang::Result;
     use proptest::prelude::*;
-    use uxd::utils::compute_value_for_shares_amount;
+    use uxd::utils::compute_value_for_shares_amount_floor;
 
     #[test]
     fn test_correctness() -> Result<()> {
         // Test regular cases and check correct rounding
-        assert_eq!(compute_value_for_shares_amount(0, 0, 0)?, 0);
-        assert_eq!(compute_value_for_shares_amount(0, 10, 0)?, 0);
-        assert_eq!(compute_value_for_shares_amount(0, 0, 10)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 0, 0)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 10, 0)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 0, 10)?, 0);
 
-        assert_eq!(compute_value_for_shares_amount(0, 100, 100)?, 0);
-        assert_eq!(compute_value_for_shares_amount(1, 100, 100)?, 1);
-        assert_eq!(compute_value_for_shares_amount(99, 100, 100)?, 99);
-        assert_eq!(compute_value_for_shares_amount(100, 100, 100)?, 100);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 100, 100)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(1, 100, 100)?, 1);
+        assert_eq!(compute_value_for_shares_amount_floor(99, 100, 100)?, 99);
+        assert_eq!(compute_value_for_shares_amount_floor(100, 100, 100)?, 100);
 
-        assert_eq!(compute_value_for_shares_amount(0, 100, 1)?, 0);
-        assert_eq!(compute_value_for_shares_amount(1, 100, 1)?, 0);
-        assert_eq!(compute_value_for_shares_amount(99, 100, 1)?, 0);
-        assert_eq!(compute_value_for_shares_amount(100, 100, 1)?, 1);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 100, 1)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(1, 100, 1)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(99, 100, 1)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(100, 100, 1)?, 1);
 
-        assert_eq!(compute_value_for_shares_amount(0, 10, 100)?, 0);
-        assert_eq!(compute_value_for_shares_amount(1, 10, 100)?, 10);
-        assert_eq!(compute_value_for_shares_amount(9, 10, 100)?, 90);
-        assert_eq!(compute_value_for_shares_amount(10, 10, 100)?, 100);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 10, 100)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(1, 10, 100)?, 10);
+        assert_eq!(compute_value_for_shares_amount_floor(9, 10, 100)?, 90);
+        assert_eq!(compute_value_for_shares_amount_floor(10, 10, 100)?, 100);
 
-        assert_eq!(compute_value_for_shares_amount(0, 100, 10)?, 0);
-        assert_eq!(compute_value_for_shares_amount(1, 100, 10)?, 0);
-        assert_eq!(compute_value_for_shares_amount(49, 100, 10)?, 4);
-        assert_eq!(compute_value_for_shares_amount(50, 100, 10)?, 5);
-        assert_eq!(compute_value_for_shares_amount(51, 100, 10)?, 5);
-        assert_eq!(compute_value_for_shares_amount(100, 100, 10)?, 10);
+        assert_eq!(compute_value_for_shares_amount_floor(0, 100, 10)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(1, 100, 10)?, 0);
+        assert_eq!(compute_value_for_shares_amount_floor(49, 100, 10)?, 4);
+        assert_eq!(compute_value_for_shares_amount_floor(50, 100, 10)?, 5);
+        assert_eq!(compute_value_for_shares_amount_floor(51, 100, 10)?, 5);
+        assert_eq!(compute_value_for_shares_amount_floor(100, 100, 10)?, 10);
 
         // Test large amounts against u64 overflow
         assert_eq!(
-            compute_value_for_shares_amount(
+            compute_value_for_shares_amount_floor(
                 1_000_000_000_000_000_000,
                 2_000_000_000_000_000_000,
                 4_000_000_000_000_000_000
@@ -44,29 +44,41 @@ mod test_compute_value_for_shares_amount {
             2_000_000_000_000_000_000
         );
         assert_eq!(
-            compute_value_for_shares_amount(u64::MAX, u64::MAX, u64::MAX)?,
+            compute_value_for_shares_amount_floor(u64::MAX, u64::MAX, u64::MAX)?,
             u64::MAX
         );
-        assert_eq!(compute_value_for_shares_amount(42, u64::MAX, u64::MAX)?, 42);
-        assert_eq!(compute_value_for_shares_amount(u64::MAX, u64::MAX, 42)?, 42);
-        assert_eq!(compute_value_for_shares_amount(42, 42, u64::MAX)?, u64::MAX);
-        assert_eq!(compute_value_for_shares_amount(u64::MAX, 42, 42)?, u64::MAX);
+        assert_eq!(
+            compute_value_for_shares_amount_floor(42, u64::MAX, u64::MAX)?,
+            42
+        );
+        assert_eq!(
+            compute_value_for_shares_amount_floor(u64::MAX, u64::MAX, 42)?,
+            42
+        );
+        assert_eq!(
+            compute_value_for_shares_amount_floor(42, 42, u64::MAX)?,
+            u64::MAX
+        );
+        assert_eq!(
+            compute_value_for_shares_amount_floor(u64::MAX, 42, 42)?,
+            u64::MAX
+        );
 
         Ok(())
     }
 
     #[test]
     fn test_incorrectness() -> Result<()> {
-        assert!(compute_value_for_shares_amount(1, 0, 0).is_err());
-        assert!(compute_value_for_shares_amount(1, 0, 10).is_err());
-        assert!(compute_value_for_shares_amount(1, 10, 0).is_err());
+        assert!(compute_value_for_shares_amount_floor(1, 0, 0).is_err());
+        assert!(compute_value_for_shares_amount_floor(1, 0, 10).is_err());
+        assert!(compute_value_for_shares_amount_floor(1, 10, 0).is_err());
         Ok(())
     }
 
     #[test]
     fn test_panic_cases() -> Result<()> {
         proptest!(|(shares_amount: u64, total_shares_supply: u64, total_shares_value: u64)| {
-            let result = compute_value_for_shares_amount(
+            let result = compute_value_for_shares_amount_floor(
                 shares_amount,
                 total_shares_supply,
                 total_shares_value
