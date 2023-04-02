de48c23d8b6129969820f95ee320b97a4cd5bf9c
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-02 17:03:58 +0900

    added-prop-test

--- programs/uxd/tests/unit_tests/utils/calculate_depositories_targets.rs
+++ programs/uxd/tests/unit_tests/utils/calculate_depositories_targets.rs
@@ -3,7 +3,7 @@
 mod test_calculate_depositories_targets {
     use anchor_lang::Result;
     use proptest::prelude::*;
-    use uxd::utils::calculate_depositories_targets;
+    use uxd::{utils::calculate_depositories_targets, BPS_UNIT_CONVERSION};
 
     fn percent_of_supply(percent: u64, supply: u64) -> u64 {
         return supply * percent / 100;
@@ -97,21 +97,53 @@ mod test_calculate_depositories_targets {
         Ok(())
     }
 
-    /*
     #[test]
     fn test_no_panic() -> Result<()> {
-        proptest!(|(amount: u64, bps: u8)| {
-            prop_assert!(calculate_depositories_targets(amount, bps).is_ok());
-        });
-        Ok(())
-    }
+        proptest!(|(
+            circulating_supply: u64,
+            identity_depository_weight_bps: u16,
+            mercurial_vault_depository_0_weight_bps: u16,
+            credix_lp_depository_0_weight_bps: u16,
+            identity_depository_hard_cap: u64,
+            mercurial_vault_depository_0_hard_cap: u64,
+            credix_lp_depository_0_hard_cap: u64,
+        )| {
+            // Compute
+            let result = calculate_depositories_targets(
+                circulating_supply.into(),
+                identity_depository_weight_bps,
+                mercurial_vault_depository_0_weight_bps,
+                credix_lp_depository_0_weight_bps,
+                identity_depository_hard_cap.into(),
+                mercurial_vault_depository_0_hard_cap.into(),
+                credix_lp_depository_0_hard_cap.into(),
+            );
 
-    #[test]
-    fn test_no_increase() -> Result<()> {
-        proptest!(|(amount: u64, bps: u8)| {
-            prop_assert!(calculate_depositories_targets(amount, bps)? <= amount);
+            // We dont support the case where the total weights exceed 100%
+            // As this could lead to overflows if we specify insane values
+            let total_weight_bps = identity_depository_weight_bps
+                + mercurial_vault_depository_0_weight_bps
+                + credix_lp_depository_0_weight_bps;
+            if u64::from(total_weight_bps) >= BPS_UNIT_CONVERSION {
+                return Ok(());
+            }
+
+            // Everything else should never panic
+            prop_assert!(result.is_ok());
+
+            // The sum of all depositories targets should always be equal to:
+            // either the circulating supply or the sum of all depositories caps
+            let total_hard_caps = identity_depository_hard_cap
+                + mercurial_vault_depository_0_hard_cap
+                + credix_lp_depository_0_hard_cap;
+
+            let depositories_targets = result?;
+            let total_target_amount = depositories_targets.identity_depository_target_amount
+                + depositories_targets.mercurial_vault_depository_0_target_amount
+                + depositories_targets.credix_lp_depository_0_target_amount;
+
+            prop_assert!(total_target_amount == std::cmp::min(total_hard_caps, circulating_supply));
         });
         Ok(())
     }
-     */
 }
