181027cbbd28e50cd1c7f9ce8f36e36ed74fdef9
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-13 18:28:42 +0900

    update-api-and-binaries

--- programs/uxd/tests/integration_tests/api/program_credix/binaries/dump.sh
+++ programs/uxd/tests/integration_tests/api/program_credix/binaries/dump.sh
@@ -1,8 +1,5 @@
 solana config set --url https://api.devnet.solana.com
-solana program dump CRdXwuY984Au227VnMJ2qvT7gPd83HwARYXcbHfseFKC executable-devnet-public.so
-
-solana config set --url https://api.devnet.solana.com
-solana program dump CRDx2YkdtYtGZXGHZ59wNv1EwKHQndnRc1gT4p8i2vPX executable-devnet-private.so
+solana program dump CRDx2YkdtYtGZXGHZ59wNv1EwKHQndnRc1gT4p8i2vPX executable-devnet.so
 
 solana config set --url https://api.mainnet-beta.solana.com
 solana program dump CRDx2YkdtYtGZXGHZ59wNv1EwKHQndnRc1gT4p8i2vPX executable-mainnet-beta.so
