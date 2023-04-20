231733a4ca1ee973def59822b199fc6127afb3be
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-20 19:06:17 +0900

    utils-updated

--- programs/uxd/tests/integration_tests/api/program_test_context/process_instruction.rs
+++ programs/uxd/tests/integration_tests/api/program_test_context/process_instruction.rs
@@ -1,4 +1,5 @@
 use solana_program::instruction::Instruction;
+use solana_program_test::BanksTransactionResultWithMetadata;
 use solana_program_test::ProgramTestContext;
 use solana_sdk::signature::Keypair;
 use solana_sdk::signer::Signer;
@@ -9,7 +10,7 @@ use crate::integration_tests::api::program_test_context;
 async fn process_instruction_result(
     program_test_context: &mut ProgramTestContext,
     instruction: Instruction,
-    result: Result<(), program_test_context::ProgramTestError>,
+    raw_result: Result<BanksTransactionResultWithMetadata, program_test_context::ProgramTestError>,
 ) -> Result<(), program_test_context::ProgramTestError> {
     // Increment the blockhash, so that the next transaction can run sequentially
     program_test_context.last_blockhash = program_test_context
@@ -23,7 +24,6 @@ async fn process_instruction_result(
         instruction.program_id.to_string()
     );
     println!(" - instruction.data: {:?}", instruction.data);
-    println!(" - instruction.result: {:?}", result);
     // Log the callers for quickly glace over the flow of IXs using minified backtrace
     let backtrace_data = std::backtrace::Backtrace::force_capture();
     let backtrace_formatted = std::format!("{}", backtrace_data);
@@ -35,8 +35,24 @@ async fn process_instruction_result(
             println!(" - instruction.from: {}", backtrace_line.trim());
         }
     }
-    // Done
-    result
+    // Done, try to print result and metadata
+    if raw_result.is_ok() {
+        let result_with_metadata = raw_result.unwrap();
+        let metadata_option = result_with_metadata.metadata;
+        if metadata_option.is_some() {
+            let metadata = metadata_option.unwrap();
+            println!(
+                " - instruction.compute_units_consumed: {:?}",
+                metadata.compute_units_consumed
+            );
+            println!(" - instruction.return_data: {:?}", metadata.return_data);
+        }
+        result_with_metadata
+            .result
+            .map_err(program_test_context::ProgramTestError::Transaction)
+    } else {
+        Err(raw_result.unwrap_err())
+    }
 }
 
 pub async fn process_instruction(
@@ -47,12 +63,12 @@ pub async fn process_instruction(
     let mut transaction: Transaction =
         Transaction::new_with_payer(&[instruction.clone()], Some(&payer.pubkey()));
     transaction.partial_sign(&[payer], program_test_context.last_blockhash);
-    let result = program_test_context
+    let raw_result = program_test_context
         .banks_client
-        .process_transaction(transaction)
+        .process_transaction_with_metadata(transaction)
         .await
         .map_err(program_test_context::ProgramTestError::BanksClient);
-    process_instruction_result(program_test_context, instruction.clone(), result).await
+    process_instruction_result(program_test_context, instruction.clone(), raw_result).await
 }
 
 pub async fn process_instruction_with_signer(
@@ -64,10 +80,10 @@ pub async fn process_instruction_with_signer(
     let mut transaction: Transaction =
         Transaction::new_with_payer(&[instruction.clone()], Some(&payer.pubkey()));
     transaction.partial_sign(&[payer, signer], program_test_context.last_blockhash);
-    let result = program_test_context
+    let raw_result = program_test_context
         .banks_client
-        .process_transaction(transaction)
+        .process_transaction_with_metadata(transaction)
         .await
         .map_err(program_test_context::ProgramTestError::BanksClient);
-    process_instruction_result(program_test_context, instruction.clone(), result).await
+    process_instruction_result(program_test_context, instruction.clone(), raw_result).await
 }
