e81d3e77659c6774510e7fca6f11eb835146e67a
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-21 12:21:54 +0900

    up

--- programs/uxd/tests/unit_tests/utils/mod.rs
+++ programs/uxd/tests/unit_tests/utils/mod.rs
@@ -1,6 +1,6 @@
 mod calculate_amount_less_fees;
 mod calculate_depositories_mint_collateral_amount;
-mod calculate_depositories_redeem_redeemable_amount;
+mod calculate_depositories_redeemable_amount;
 mod calculate_depositories_sum_value;
 mod calculate_depositories_target_redeemable_amount;
 mod math;
