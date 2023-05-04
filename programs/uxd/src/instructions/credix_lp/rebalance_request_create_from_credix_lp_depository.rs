aef5ae0547bae9cb4d140e083cf4adce46fe6de9
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-05-04 17:20:03 +0900

    better-annotation-check-for-withdraw-request

--- programs/uxd/src/instructions/credix_lp/rebalance_request_create_from_credix_lp_depository.rs
+++ programs/uxd/src/instructions/credix_lp/rebalance_request_create_from_credix_lp_depository.rs
@@ -13,6 +13,7 @@ use crate::CONTROLLER_NAMESPACE;
 use crate::CREDIX_LP_DEPOSITORY_NAMESPACE;
 use crate::CREDIX_LP_EXTERNAL_PASS_NAMESPACE;
 use crate::CREDIX_LP_EXTERNAL_WITHDRAW_EPOCH_NAMESPACE;
+use crate::CREDIX_LP_EXTERNAL_WITHDRAW_REQUEST_NAMESPACE;
 
 #[derive(Accounts)]
 pub struct RebalanceRequestCreateFromCredixLpDepository<'info> {
@@ -101,8 +102,17 @@ pub struct RebalanceRequestCreateFromCredixLpDepository<'info> {
     pub credix_withdraw_epoch: Account<'info, credix_client::WithdrawEpoch>,
 
     /// #12
-    /// CHECK: not used by us, created by credix program
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [
+            credix_global_market_state.key().as_ref(),
+            depository.key().as_ref(),
+            &credix_global_market_state.latest_withdraw_epoch_idx.to_le_bytes(),
+            CREDIX_LP_EXTERNAL_WITHDRAW_REQUEST_NAMESPACE
+        ],
+        bump,
+        seeds::program = credix_client::ID,
+    )]
     pub credix_withdraw_request: AccountInfo<'info>,
 
     /// #13
