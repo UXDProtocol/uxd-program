3f7f77643ca9abd8494fc5bffcbedb5640b585ed
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-07-03 17:09:35 +0900

    added-script

--- scripts/trigger_rebalance_credix.js
+++ scripts/trigger_rebalance_credix.js
@@ -1,21 +1,19 @@
-import {
+const {
   Controller,
   CredixLpDepository,
   IdentityDepository,
   MercurialVaultDepository,
   UXDClient,
-} from '@uxd-protocol/uxd-client';
-import {
-  Commitment,
+} = require('@uxd-protocol/uxd-client');
+const {
   Connection,
   Keypair,
   PublicKey,
-  Signer,
   Transaction,
-} from '@solana/web3.js';
-import { web3 } from '@project-serum/anchor';
+} = require('@solana/web3.js');
+const { web3 } = require('@project-serum/anchor');
 
-const TXN_COMMIT: Commitment = 'confirmed';
+const TXN_COMMIT = 'confirmed';
 const connectionConfig = {
   commitment: TXN_COMMIT,
   disableRetryOnRateLimit: false,
@@ -32,7 +30,7 @@ const uxdProgramId = new PublicKey(
 );
 const usdcMint = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
 
-function getConnection(): Connection {
+function getConnection() {
   const connection = new Connection(
     'https://api.mainnet-beta.solana.com',
     connectionConfig
@@ -40,11 +38,11 @@ function getConnection(): Connection {
   return connection;
 }
 
-function createIdentityDepository(): IdentityDepository {
+function createIdentityDepository() {
   return new IdentityDepository(usdcMint, 'USDC', 6, uxdProgramId);
 }
 
-async function createMercurialVaultDepository(): Promise<MercurialVaultDepository> {
+async function createMercurialVaultDepository() {
   try {
     return await MercurialVaultDepository.initialize({
       connection: getConnection(),
@@ -62,7 +60,7 @@ async function createMercurialVaultDepository(): Promise<MercurialVaultDepositor
   }
 }
 
-async function createCredixLpDepository(): Promise<CredixLpDepository> {
+async function createCredixLpDepository() {
   try {
     return await CredixLpDepository.initialize({
       connection: getConnection(),
@@ -80,26 +78,34 @@ async function createCredixLpDepository(): Promise<CredixLpDepository> {
 }
 
 async function main() {
-  const controller = new Controller('UXD', 6, uxdProgramId);
-
-  const identityDepository = createIdentityDepository();
-  const mercurialVaultDepository = await createMercurialVaultDepository();
-  const credixLpDepository = await createCredixLpDepository();
-
-  credixLpDepository.info();
-
+  // Dummy payer for mainnet tooling E7N44oZ3APNFjzv95xL6kSxSLgw3wVP3ixM7dgsMApzZ
   const payer = Keypair.fromSeed(
     Uint8Array.from([
       1, 56, 76, 89, 32, 55, 1, 128, 98, 23, 56, 22, 30, 12, 76, 23, 2, 9, 3, 5,
       1, 22, 120, 109, 0, 8, 5, 3, 2, 7, 6, 8,
     ])
   );
+  console.log('payer', payer.publicKey.toBase58());
+
+  const controller = new Controller('UXD', 6, uxdProgramId);
+  const identityDepository = createIdentityDepository();
+  const mercurialVaultDepository = await createMercurialVaultDepository();
+  const credixLpDepository = await createCredixLpDepository();
+
+  controller.info();
+  identityDepository.info();
+  mercurialVaultDepository.info();
+  credixLpDepository.info();
+
+  const profitsBeneficiaryCollateral = (
+    await credixLpDepository.getOnchainAccount(getConnection(), TXN_OPTS)
+  ).profitsBeneficiaryCollateral;
+  console.log(
+    'profitsBeneficiaryCollateral',
+    profitsBeneficiaryCollateral.toBase58()
+  );
 
   const uxdClient = new UXDClient(uxdProgramId);
-
-  const profitsBeneficiaryCollateral = new PublicKey(
-    'EbmvhGxvvMQhCx8PrWRHScMTbq6tMuwdeigh5muT4bwr'
-  );
 
   const rebalanceCreateInstruction =
     uxdClient.createRebalanceCreateWithdrawRequestFromCredixLpDepositoryInstruction(
@@ -110,6 +116,22 @@ async function main() {
       payer.publicKey,
       TXN_OPTS
     );
+  const rebalanceCreateTransaction = new Transaction();
+  rebalanceCreateTransaction.add(rebalanceCreateInstruction);
+  rebalanceCreateTransaction.feePayer = payer.publicKey;
+  console.log('rebalanceCreateTransaction', rebalanceCreateTransaction);
+
+  try {
+    const rebalanceCreateResult = await web3.sendAndConfirmTransaction(
+      getConnection(),
+      rebalanceCreateTransaction,
+      [payer],
+      TXN_OPTS
+    );
+    console.log('rebalanceCreateResult', rebalanceCreateResult);
+  } catch (rebalanceCreateError) {
+    console.log('rebalanceCreateError', rebalanceCreateError);
+  }
 
   const rebalanceRedeemInstruction =
     uxdClient.createRebalanceRedeemWithdrawRequestFromCredixLpDepositoryInstruction(
@@ -121,23 +143,22 @@ async function main() {
       profitsBeneficiaryCollateral,
       TXN_OPTS
     );
+  const rebalanceRedeemTransaction = new Transaction();
+  rebalanceRedeemTransaction.add(rebalanceRedeemInstruction);
+  rebalanceRedeemTransaction.feePayer = payer.publicKey;
+  console.log('rebalanceRedeemTransaction', rebalanceRedeemTransaction);
 
-  const signers: Signer[] = [payer];
-  const transaction = new Transaction();
-  transaction.add(rebalanceRedeemInstruction);
-  transaction.feePayer = payer.publicKey;
-
-  console.log('payer', payer.publicKey.toBase58());
-  console.log('signers', signers);
-  console.log('transaction', transaction);
-
-  const result = await web3.sendAndConfirmTransaction(
-    getConnection(),
-    transaction,
-    signers,
-    TXN_OPTS
-  );
-  console.log('result', result);
+  try {
+    const rebalanceRedeemResult = await web3.sendAndConfirmTransaction(
+      getConnection(),
+      rebalanceCreateTransaction,
+      [payer],
+      TXN_OPTS
+    );
+    console.log('rebalanceRedeemResult', rebalanceRedeemResult);
+  } catch (rebalanceRedeemError) {
+    console.log('rebalanceRedeemError', rebalanceRedeemError);
+  }
 }
 
 main();
