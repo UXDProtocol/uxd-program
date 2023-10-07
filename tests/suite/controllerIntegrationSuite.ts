847d35cbe5ff3f874ee6b06663c3081ce8b7daa3
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-10-07 15:00:36 +0900

    set-router-depositories-in-ts-tests

--- tests/suite/controllerIntegrationSuite.ts
+++ tests/suite/controllerIntegrationSuite.ts
@@ -1,6 +1,10 @@
 import { Signer } from '@solana/web3.js';
 import { Controller } from '@uxd-protocol/uxd-client';
 import { initializeControllerTest } from '../cases/initializeControllerTest';
+import { editControllerTest } from '../cases/editControllerTest';
+import { createIdentityDepositoryDevnet } from '../utils';
+import { createCredixLpDepositoryDevnetUSDC } from '../utils';
+import { createMercurialVaultDepositoryDevnet } from '../utils';
 
 export class controllerIntegrationSuiteParameters {
   public globalSupplyCap: number;
@@ -25,4 +29,22 @@ export const controllerIntegrationSuite = function ({
       controller,
       payer,
     }));
+
+  it('Initialize router depositories', async function () {
+    const identityDepository = createIdentityDepositoryDevnet();
+    const credixLpDepository = await createCredixLpDepositoryDevnetUSDC();
+    const mercurialVaultDepository =
+      await createMercurialVaultDepositoryDevnet();
+    editControllerTest({
+      authority,
+      controller,
+      uiFields: {
+        routerDepositories: {
+          identityDepository: identityDepository.pda,
+          mercurialVaultDepository: mercurialVaultDepository.pda,
+          credixLpDepository: credixLpDepository.pda,
+        },
+      },
+    });
+  });
 };
