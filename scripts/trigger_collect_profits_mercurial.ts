e7e027b5b9aa5a00cc602f921e9c7ede5bead35d
cnek <ctk2012ac@gmail.com>
2023-10-28 00:49:36 +0800

    moved payer out

--- scripts/trigger_collect_profits_mercurial.ts
+++ scripts/trigger_collect_profits_mercurial.ts
@@ -1,24 +1,16 @@
 import { web3 } from '@project-serum/anchor';
 import { amountToUiAmount } from '@solana/spl-token';
-import { Keypair, Transaction } from '@solana/web3.js';
+import { Transaction } from '@solana/web3.js';
 import { Controller, UXDClient } from '@uxd-protocol/uxd-client';
 import {
   createMercurialVaultDepository,
   getConnection,
+  payer,
   TXN_OPTS,
   uxdProgramId,
 } from './common';
 
 async function main() {
-  // Dummy payer for mainnet tooling E7N44oZ3APNFjzv95xL6kSxSLgw3wVP3ixM7dgsMApzZ
-  const payer = Keypair.fromSeed(
-    Uint8Array.from([
-      1, 56, 76, 89, 32, 55, 1, 128, 98, 23, 56, 22, 30, 12, 76, 23, 2, 9, 3, 5,
-      1, 22, 120, 109, 0, 8, 5, 3, 2, 7, 6, 8,
-    ])
-  );
-  console.log('payer', payer.publicKey.toBase58());
-
   const controller = new Controller('UXD', 6, uxdProgramId);
   const depository = await createMercurialVaultDepository();
 
