8247e614d4c7935f0bb0762f7e3ec7eebf0502aa
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-12-24 13:07:32 +0100

    mercurial-check

--- programs/uxd/src/instructions/mercurial/collect_profits_of_mercurial_vault_depository.rs
+++ programs/uxd/src/instructions/mercurial/collect_profits_of_mercurial_vault_depository.rs
@@ -120,14 +120,16 @@ pub(crate) fn handler(ctx: Context<CollectProfitsOfMercurialVaultDepository>) ->
     ]];
 
     // 5 - withdraw collateral from mercurial vault for LP tokens
-    mercurial_vault::cpi::withdraw(
-        ctx.accounts
-            .into_withdraw_collateral_from_mercurial_vault_context()
-            .with_signer(depository_signer_seed),
-        lp_token_amount_to_match_collectable_profits_value,
-        // Do not check slippage here
-        0,
-    )?;
+    if lp_token_amount_to_match_collectable_profits_value > 0 {
+        mercurial_vault::cpi::withdraw(
+            ctx.accounts
+                .into_withdraw_collateral_from_mercurial_vault_context()
+                .with_signer(depository_signer_seed),
+            lp_token_amount_to_match_collectable_profits_value,
+            // Do not check slippage here
+            0,
+        )?;
+    }
 
     // 6 - Reload accounts impacted by the withdraw (We need updated numbers for further calculation)
     ctx.accounts.depository_lp_token_vault.reload()?;
